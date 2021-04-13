1dc464f636f1a508bd3cba98cb106d2e92a435d6
Charles Zoeller <charles.zoeller1@gmail.com>
2021-04-12 22:33:11 -0600

    refactored Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,50 @@
 import React from "react";
 import resume from "../assets/Resume.pdf";
 
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import Box from "@material-ui/core/Box";
+import { useTheme } from "@material-ui/core/styles";
+
 function Footer() {
+  const theme = useTheme();
   return (
-    <footer className="footer mt-auto fixed-bottom py-3 bg-dark">
-      <div className="container">
-        <div className="text-white">
-          Charles Zoeller{" "}
-          <a href={resume} target="blank" className="text-muted">
-            Resume
-          </a>{" "}
-          <span className="text-muted">*</span>{" "}
-          <a
-            href="https://github.com/czoeller1"
-            target="blank"
-            className="text-muted"
-          >
-            Git Hub
-          </a>{" "}
-          <span className="text-muted">*</span>{" "}
-          <a
-            href="https://www.linkedin.com/in/charles-zoeller/"
-            target="blank"
-            className="text-muted"
-          >
-            LinkedIn
-          </a>{" "}
-        </div>
-      </div>
-    </footer>
+    <>
+      <AppBar
+        color="primary"
+        position="fixed"
+        style={{ top: "auto", bottom: 0 }}
+      >
+        <Box mx="auto">
+          <Toolbar>
+            <Button
+              style={{ color: theme.palette.secondary.main }}
+              href="https://github.com/czoeller1"
+              target="blank"
+            >
+              GitHub
+            </Button>
+            <Button
+              style={{ color: theme.palette.secondary.main }}
+              href={resume}
+              target="blank"
+            >
+              Resume
+            </Button>
+            <Button
+              style={{ color: theme.palette.secondary.main }}
+              href="https://www.linkedin.com/in/charles-zoeller/"
+              target="blank"
+            >
+              LinkedIn
+            </Button>
+          </Toolbar>
+        </Box>
+      </AppBar>
+      <Toolbar />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,43 +16,6 @@ function Navbar() {
 
   return (
     <>
-      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-end">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">
-            Charles Zoeller
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
-                  Contact
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/projects">
-                  Portfolio
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" exact to="/">
-                  About
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> */}
       <AppBar color="primary" position="sticky">
         <Toolbar>
           <Box pr="2rem">
