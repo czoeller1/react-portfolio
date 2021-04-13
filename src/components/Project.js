642e39894b206bc32a3eed1bc6a041ffbe53e442
Charles Zoeller <charles.zoeller1@gmail.com>
2021-04-12 23:56:04 -0600

    refactored portfolio/project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,33 +1,65 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
-function Project(props) {
+function Project({ classes, ...props }) {
   return (
-    <div className="col-6">
-      <div className="card">
-        <h4 className="card-title text-center my-3">{props.title}</h4>
-        <img
-          src={props.image}
-          className="card-img-top border border-2"
-          alt={`${props.title} screenshot`}
-        />
-        <div className="card-body">
-          <p className="card-text">{props.description}</p>
-          <div className="row d-flex justify-content-around">
-            <a href={props.site} target="blank" className="btn btn-info col-3">
-              View Site
-            </a>
+    <Grid item xs justify="center" style={{ margin: "2%" }}>
+      <Paper className={classes.paper}>
+        <Grid item>
+          <Typography variant="h3" gutterBottom>
+            {props.title}
+          </Typography>
+        </Grid>
 
-            <a
-              href={props.repository}
-              target="blank"
-              className="btn btn-info col-3"
-            >
-              View Repository
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+        <hr />
+
+        <Grid item>
+          <img className={classes.img} alt="complex" src={props.image} />
+        </Grid>
+
+        <Grid container>
+          <Grid item>
+            <Typography gutterBottom variant="subtitle1">
+              {props.description}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="caption">{props.details}</Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container style={{ flexGrow: 1, marginTop: 2 }} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify="space-evenly" spacing={3}>
+              <Grid item>
+                <Button
+                  href={props.site}
+                  variant="contained"
+                  target="blank"
+                  color="secondary"
+                >
+                  View Site
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  href={props.repository}
+                  target="blank"
+                  variant="contained"
+                  color="secondary"
+                >
+                  View Repository
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
   );
 }
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,74 +6,115 @@ import planner from "../assets/planner.png";
 import adventureops from "../assets/adventureops.png";
 import notes from "../assets/notes.png";
 import herald from "../assets/herald.png";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: "75%",
+    margin: "auto",
+  },
+  title: {
+    padding: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    height: "100%",
+  },
+  image: {
+    width: 200,
+    //height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
 
 function Portfolio() {
+  const classes = useStyles();
   return (
-    <main className="container bg-light ms-3 my-5 pb-4 w-75">
-      <div className="row text-center">
-        <h1>Portfolio</h1>
-        <p>
-          Some projects are hosted on heroku, which can be unstable and slow to
-          start
-        </p>
-      </div>
-      <div class="row mx-auto my-3">
-        <Project
-          title="Urban El Dorado"
-          image={urban}
-          description="A project that lets users search for a city and receive a map
-                accompanied by selected US Census data."
-          site="https://pwg26.github.io/RepositoryForAwesomeProject/"
-          repository="https://github.com/pwg26/RepositoryForAwesomeProject"
-        />
-        <Project
-          title="Weather Forecast"
-          image={weather}
-          description=" A project that uses the OpenWeather API to display current and
+    <main className={classes.root}>
+      <Box ms={3} my={5} p={3}>
+        <Grid container spacing={3}>
+          <Grid item xs justify="center" style={{ margin: "2%" }}>
+            <Paper className={classes.title} style={{ textAlign: "center" }}>
+              <Typography variant="h3">Portfolio</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={3} style={{ height: "100%" }}>
+          <Project
+            classes={classes}
+            title="Urban El Dorado"
+            image={urban}
+            description="A project that lets users search for a city and receive a map
+                        accompanied by selected US Census data."
+            site="https://pwg26.github.io/RepositoryForAwesomeProject/"
+            repository="https://github.com/pwg26/RepositoryForAwesomeProject"
+          />
+          <Project
+            classes={classes}
+            title="Weather Forecast"
+            image={weather}
+            description=" A project that uses the OpenWeather API to display current and
           future weather conditions for the searched city."
-          site="https://czoeller1.github.io/weather-forecast/"
-          repository="https://github.com/czoeller1/weather-forecast"
-        />
-      </div>
-      <div class="row mx-auto my-3">
-        <Project
-          title="Day Planner"
-          image={planner}
-          description="A project that creates an hourly planner for the current day.
+            site="https://czoeller1.github.io/weather-forecast/"
+            repository="https://github.com/czoeller1/weather-forecast"
+          />
+        </Grid>
+        <Grid container spacing={3} style={{ height: "100%" }}>
+          <Project
+            classes={classes}
+            title="Day Planner"
+            image={planner}
+            description="A project that creates an hourly planner for the current day.
           The entries are color coded according to the current time."
-          site="https://czoeller1.github.io/day-planner/"
-          repository="https://github.com/czoeller1/day-planner"
-        />
-        <Project
-          title="Adventureops"
-          image={adventureops}
-          description="A full stack application for outdoor enthusiasts to post about
+            site="https://czoeller1.github.io/day-planner/"
+            repository="https://github.com/czoeller1/day-planner"
+          />
+          <Project
+            classes={classes}
+            title="Adventureops"
+            image={adventureops}
+            description="A full stack application for outdoor enthusiasts to post about
           various locations and activities. It allows users to create an
           account and make posts for the currently limited places and
           activities. Future development goals include a chat feature,
           user photo integration, and the ability for users to add
           locations or activities. Users can also see a page with all of
           their posts that allows them to delete their posts."
-          site="https://adventureops.herokuapp.com/login"
-          repository="https://github.com/Perkyderm/Adventureops"
-        />
-      </div>
-      <div class="row mx-auto my-3">
-        <Project
-          title="Note Taker"
-          image={notes}
-          description=" page that allows the user to create, save, and view custom notes with a title and text. The notes are stored in the server's backend and persist between sessions. Notes can be deleted through the interface."
-          site="https://frozen-hollows-50239.herokuapp.com/"
-          repository="https://github.com/czoeller1/note-taker"
-        />
-        <Project
-          title="Hospitality Herald"
-          image={herald}
-          description={` A hospitality management system that allows users to create rooms, store items, and activities, all of which can be assigned to guests entered into the system. The site uses authentication to protect guest and user information. Demo username: 'charles' password: 'testpwd'`}
-          site="https://hostelhedonism.herokuapp.com/"
-          repository="https://github.com/pwg26/hostel_hedonism"
-        />
-      </div>
+            site="https://adventureops.herokuapp.com/login"
+            repository="https://github.com/Perkyderm/Adventureops"
+          />
+        </Grid>
+        <Grid container spacing={3} style={{ height: "70%" }}>
+          <Project
+            classes={classes}
+            title="Note Taker"
+            image={notes}
+            description=" page that allows the user to create, save, and view custom notes with a title and text. The notes are stored in the server's backend and persist between sessions. Notes can be deleted through the interface."
+            site="https://frozen-hollows-50239.herokuapp.com/"
+            repository="https://github.com/czoeller1/note-taker"
+          />
+          <Project
+            classes={classes}
+            title="Hospitality Herald"
+            image={herald}
+            description={` A hospitality management system that allows users to create rooms, store items, and activities, all of which can be assigned to guests entered into the system. The site uses authentication to protect guest and user information.`}
+            details={`Demo username: 'charles' password: 'testpwd'`}
+            site="https://hostelhedonism.herokuapp.com/"
+            repository="https://github.com/pwg26/hostel_hedonism"
+          />
+        </Grid>
+      </Box>
     </main>
   );
 }
