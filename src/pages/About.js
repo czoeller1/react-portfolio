f9797c2614c47f5e8eff187400b7acf19a472ef8
Charles Zoeller <charles.zoeller1@gmail.com>
2021-04-12 22:12:20 -0600

    refactored about page to material-ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,70 @@
 import React from "react";
 import portrait from "../assets/portrait.jpg";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: "75%",
+  },
+  image: {
+    width: 200,
+    //height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
 
 function About() {
-  return (
-    <main className="flex-shrink-0 w-75">
-      <div className="container bg-light ms-3 my-5 p-3">
-        <div className="row">
-          <h1>About Me</h1>
-        </div>
+  const classes = useStyles();
 
-        <div className="card">
-          <div className="card-body" style={{ lineHeight: "150%" }}>
-            <p>
-              <img
-                src={portrait}
-                alt="a portrait of myself"
-                className="card-img-top float-start px-3"
-                style={{ width: "200px" }}
-              />
-              My name is Charles Zoeller and I'm a born native of Colorado. I
-              have skiied for as long as I can remember, and enjoy hitting the
-              slopes with family and friends. I really enjoy reading science
-              fiction and fantasy novels. My favorite authors are Erin
-              Morgenstern, Maddeline Miller, and Brandon Sanderson. I have also
-              became interested in cooking and baking. I've made Beef
-              Wellington, Cheesecake, and Soft Pretzels.
-            </p>
-          </div>
-        </div>
-      </div>
+  return (
+    <main className={classes.root}>
+      <Box ms={3} my={5} p={3}>
+        <Paper className={classes.paper}>
+          <Grid container spacing={2}>
+            <Grid item>
+              <Typography variant="h3" gutterBottom>
+                About Me
+              </Typography>
+            </Grid>
+          </Grid>
+          <hr />
+          <Grid container spacing={2}>
+            <Grid item className={classes.image}>
+              <img className={classes.img} alt="complex" src={portrait} />
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Typography gutterBottom variant="subtitle1">
+                    My name is Charles Zoeller and I'm a born native of
+                    Colorado. I have skiied for as long as I can remember, and
+                    enjoy hitting the slopes with family and friends. I really
+                    enjoy reading science fiction and fantasy novels. My
+                    favorite authors are Erin Morgenstern, Maddeline Miller, and
+                    Brandon Sanderson. I have also became interested in cooking
+                    and baking. I've made Beef Wellington, Cheesecake, and Soft
+                    Pretzels.
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
     </main>
   );
 }
