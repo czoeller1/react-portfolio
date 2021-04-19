0b486d53fcaf07debc0f2dedf335b6dacec50b0f
Charles Zoeller <charles.zoeller1@gmail.com>
2021-04-19 14:58:30 -0600

    redesigned portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -43,7 +43,17 @@ function Portfolio() {
     <main className={classes.root}>
       <Box ms={3} my={5} p={3}>
         <Grid container spacing={3}>
-          <Grid item xs justify="center" style={{ margin: "2%" }}>
+          <Grid
+            item
+            xs
+            justify="center"
+            style={{
+              marginTop: "2%",
+              marginBottom: "2%",
+              marginLeft: "40%",
+              marginRight: "40%",
+            }}
+          >
             <Paper className={classes.title} style={{ textAlign: "center" }}>
               <Typography variant="h3">Portfolio</Typography>
             </Paper>
@@ -51,35 +61,6 @@ function Portfolio() {
         </Grid>
 
         <Grid container spacing={3} style={{ height: "100%" }}>
-          <Project
-            classes={classes}
-            title="Urban El Dorado"
-            image={urban}
-            description="A project that lets users search for a city and receive a map
-                        accompanied by selected US Census data."
-            site="https://pwg26.github.io/RepositoryForAwesomeProject/"
-            repository="https://github.com/pwg26/RepositoryForAwesomeProject"
-          />
-          <Project
-            classes={classes}
-            title="Weather Forecast"
-            image={weather}
-            description=" A project that uses the OpenWeather API to display current and
-          future weather conditions for the searched city."
-            site="https://czoeller1.github.io/weather-forecast/"
-            repository="https://github.com/czoeller1/weather-forecast"
-          />
-        </Grid>
-        <Grid container spacing={3} style={{ height: "100%" }}>
-          <Project
-            classes={classes}
-            title="Day Planner"
-            image={planner}
-            description="A project that creates an hourly planner for the current day.
-          The entries are color coded according to the current time."
-            site="https://czoeller1.github.io/day-planner/"
-            repository="https://github.com/czoeller1/day-planner"
-          />
           <Project
             classes={classes}
             title="Adventureops"
@@ -94,16 +75,6 @@ function Portfolio() {
             site="https://adventureops.herokuapp.com/login"
             repository="https://github.com/Perkyderm/Adventureops"
           />
-        </Grid>
-        <Grid container spacing={3} style={{ height: "70%" }}>
-          <Project
-            classes={classes}
-            title="Note Taker"
-            image={notes}
-            description=" page that allows the user to create, save, and view custom notes with a title and text. The notes are stored in the server's backend and persist between sessions. Notes can be deleted through the interface."
-            site="https://frozen-hollows-50239.herokuapp.com/"
-            repository="https://github.com/czoeller1/note-taker"
-          />
           <Project
             classes={classes}
             title="Hospitality Herald"
@@ -112,6 +83,45 @@ function Portfolio() {
             details={`Demo username: 'charles' password: 'testpwd'`}
             site="https://hostelhedonism.herokuapp.com/"
             repository="https://github.com/pwg26/hostel_hedonism"
+          />
+        </Grid>
+        <Grid container spacing={3} style={{ height: "100%" }}>
+          <Project
+            classes={classes}
+            title="Urban El Dorado"
+            image={urban}
+            description="A project that lets users search for a city and receive a map
+                        accompanied by selected US Census data."
+            site="https://pwg26.github.io/RepositoryForAwesomeProject/"
+            repository="https://github.com/pwg26/RepositoryForAwesomeProject"
+          />
+          <Project
+            classes={classes}
+            title="Day Planner"
+            image={planner}
+            description="A project that creates an hourly planner for the current day.
+          The entries are color coded according to the current time."
+            site="https://czoeller1.github.io/day-planner/"
+            repository="https://github.com/czoeller1/day-planner"
+          />
+        </Grid>
+        <Grid container spacing={3} style={{ height: "70%" }}>
+          <Project
+            classes={classes}
+            title="Note Taker"
+            image={notes}
+            description="A page that allows the user to create, save, and view custom notes with a title and text. The notes are stored in the server's backend and persist between sessions. Notes can be deleted through the interface."
+            site="https://frozen-hollows-50239.herokuapp.com/"
+            repository="https://github.com/czoeller1/note-taker"
+          />
+          <Project
+            classes={classes}
+            title="Weather Forecast"
+            image={weather}
+            description=" A project that uses the OpenWeather API to display current and
+          future weather conditions for the searched city."
+            site="https://czoeller1.github.io/weather-forecast/"
+            repository="https://github.com/czoeller1/weather-forecast"
           />
         </Grid>
       </Box>
